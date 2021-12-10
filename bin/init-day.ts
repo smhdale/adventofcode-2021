0c14efb22700784ab821e02318f19726f1aadefa
Sam Dale <smhdale@live.com.au>
2021-12-10 11:27:14 +1000

    Update run script, add init script for bootstrapping new days

---
+++ bin/init-day.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env ts-node-script
+
+import { existsSync, mkdirSync, openSync, closeSync } from 'fs'
+import { resolve } from 'path'
+
+const day = (process.argv[2] || '').padStart(2, '0')
+const dir = resolve(__dirname, '..', day)
+
+if (existsSync(dir)) {
+	console.log(`Day ${day} exists; nothing to do.`)
+	process.exit(0)
+}
+
+// Create directory and files for given day
+const touchSync = (file: string) => closeSync(openSync(file, 'w'))
+mkdirSync(dir)
+touchSync(resolve(dir, 'index.ts'))
+touchSync(resolve(dir, 'input.txt'))
